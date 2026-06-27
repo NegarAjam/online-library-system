5335335a0a9dbdf6f45ef3cdca5ab16b2d459ebe
Negar <negarajam81@gmail.com>
2026-06-27 13:53:04 +0330

    Create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    title = models.CharField(
+        max_length=255
+    )
+
+    author = models.CharField(
+        max_length=255
+    )
+
+    genre = models.CharField(
+        max_length=100
+    )
+
+    publication_year = models.IntegerField()
+
+    available_copies = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
