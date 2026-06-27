56c385b03c9f8f31f068655fb7f7b30ec67477ae
Negar <negarajam81@gmail.com>
2026-06-27 13:50:30 +0330

    Implement authentication and admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "role",
+        "is_staff",
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+
+    serializer_class = RegisterSerializer
